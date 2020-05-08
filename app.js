094f9b4b5825a9bc4ce6c1726c7240ab2d971680
Logan Hemphill <logan.hemphill86@gmail.com>
2020-05-08 16:40:49 -0400

    Finished inquirer, app functioning

--- app.js
+++ app.js
@@ -4,77 +4,101 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
+const employees = [];
+const filename = "./output/team.html";
 
 async function init() {
-    try {
-      const questions = await inquirer.prompt([
+  try {
+    const q = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the employee's full name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the employee's id #?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the employee's company role?",
+        name: "role",
+      },
+    ]);
+
+    if (q.role.toLowerCase() === "engineer") {
+      let engQuestions = await inquirer.prompt([
         {
-            type: "input",
-            message: "What is the employee's full name?",
-            name: "name",
-          },
-          {
-            type: "input",
-            message: "What is the employee's id #?",
-            name: "id",
-          },
-          {
-            type: "input",
-            message: "What is the employee's email?",
-            name: "email",
-          },
-          {
-            type: "input",
-            message: "What is the employee's company role?",
-            name: "role",
-          },
-        ]);
-        if (questions.role.toLowerCase() === "engineer") {
-            console.log("engineer");
-            
-        } else if (questions.role.toLowerCase() === "intern") {
-            console.log("intern");
-            
-        } else if (questions.role.toLowerCase() === "manager") {
-            console.log("manager")
-            
-        }
-        console.log(questions);
-        // const userInfo = await app.getUser(questions.username);
-        // questions.email = userInfo.email;
-        // questions.profilePic = userInfo.avatar_url;
-        // const answers = generateMarkdown(questions);
-        // fs.writeFile(filename, answers, function () {
-        //   console.log("Successfully generated README.md file!");
-        // });
-      } catch (err) {
-        console.log(err);
-      }
+          type: "input",
+          message: "What is the employee's GitHub user name?",
+          name: "github",
+        },
+      ]);
+      const engineer = new Engineer(q.name, q.id, q.email, engQuestions.github);
+      employees.push(engineer);
+    } else if (q.role.toLowerCase() === "intern") {
+      let intQuestions = await inquirer.prompt([
+        {
+          type: "input",
+          message: "What school does the intern attend?",
+          name: "school",
+        },
+      ]);
+      const intern = new Intern(q.name, q.id, q.email, intQuestions.school);
+      employees.push(intern);
+    } else if (q.role.toLowerCase() === "manager") {
+      let manQuestions = await inquirer.prompt([
+        {
+          type: "input",
+          message: "What is the Manager's office number?",
+          name: "officeNumber",
+        },
+      ]);
+      const manager = new Manager(
+        q.name,
+        q.id,
+        q.email,
+        manQuestions.officeNumber
+      );
+      employees.push(manager);
     }
+
+    let addAnother = await inquirer.prompt({
+      type: "confirm",
+      message: "Would you like to add another employee?",
+      name: "add",
+    });
+
+    const addemployee = addAnother.add;
+    if (addemployee) {
+      init();
+      return;
+    }
+
+    console.log(employees);
+    const team = render(employees);
+    fs.writeFile(filename, team, function () {
+      console.log("Successfully generated team profile page!");
+    });
     
-    init();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// ​Break
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// ​Break
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-// ​Break
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// ​Break
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
